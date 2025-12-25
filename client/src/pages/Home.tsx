4319bdf8ffef3970eb892714cce4ddc570e913df
dikie001 <omondidickens255@gmail.com>
2025-12-25 11:16:00 +0300

    refactor

--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -44,7 +44,7 @@ const Home = () => {
 
       {/* Progress Bar */}
       <motion.div
-        className="fixed top-0 left-0 right-0 h-1 bg-primary z-50 origin-left"
+        className="fixed top-0 left-0 right-0 h-1 bg- z-50 origin-left"
         style={{ scaleX }}
       />
 
