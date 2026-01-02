690c533df742faa53d109fcc42f3a5320e5d35fe
dikie001 <omondidickens255@gmail.com>
2026-01-03 00:47:09 +0300

    edited the env.development

--- server/src/config/index.ts
+++ server/src/config/index.ts
@@ -11,5 +11,5 @@ dotenv.config({
 export const config = {
   port: process.env.PORT || 5000,
   nodeEnv: process.env.NODE_ENV || "development",
-  corsOrigin: process.env.CORS_ORIGIN || "http://localhost:5173",
+  corsOrigin: process.env.CORS_ORIGIN || "http://localhost:5000",
 };
